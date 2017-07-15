6b37e3aea8bac261f92462c3bca5e19822b9ef8d
Michael Peyper <michael.peyper@ioof.com.au>
2017-07-15 23:43:14 +1000

    Removed invalid exports from react-redux-subspace index.js

--- packages/react-redux-subspace/src/index.js
+++ packages/react-redux-subspace/src/index.js
@@ -6,9 +6,5 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export { asGlobal, GlobalActions } from './actions/GlobalActions'
-export { default as namespaced } from './reducers/namespaced'
 export { default as subspaced } from './components/subspaced'
 export { default as SubspaceProvider } from './components/SubspaceProvider'
-export { default as withStore } from './sagas/withStore'
-export { default as subspacedSagas } from './sagas/subspacedSagas'
