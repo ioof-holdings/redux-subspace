a968c85609695c7f74227eaa0a31bf0925624258
Michael Peyper <michael.peyper@ioof.com.au>
2017-08-21 14:50:21 +1000

    Added redux-subspace-observable package with integration test

---
+++ packages/redux-subspace-observable/README.md
@@ -0,0 +1,18 @@
+# redux-subspace-observable
+
+[![npm version](https://img.shields.io/npm/v/redux-subspace-observable.svg?style=flat-square)](https://www.npmjs.com/package/redux-subspace-observable)
+[![npm downloads](https://img.shields.io/npm/dm/redux-subspace-observable.svg?style=flat-square)](https://www.npmjs.com/package/redux-subspace-observable)
+[![License: MIT](https://img.shields.io/npm/l/redux-subspace-observable.svg?style=flat-square)](LICENSE.md)
+
+This is a library to create subspaces for epics. It's designed to work with [redux-observable](https://redux-observable.js.org/) middleware.
+
+## Installation
+
+```sh
+npm install --save redux-subspace-observable
+```
+
+## Documentation
+
+* [Usage](/packages/redux-subspace-observable/docs/Usage.md)
+* [API Reference](/packages/redux-subspace-observable/docs/api/README.md)
---
+++ packages/redux-subspace-observable/test/integration-spec.js
@@ -0,0 +1,218 @@
+/**
+ * Copyright 2017, IOOF Holdings Limited.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { createStore, combineReducers } from 'redux'
+import { subspace, applyMiddleware, namespaced } from 'redux-subspace'
+import { combineEpics } from 'redux-observable'
+import { map } from 'rxjs/operator/map'
+import { createEpicMiddleware, subspaced } from '../src'
+
+describe('integration tests', () => {
+
+    const TEST_ACTION_TRIGGER = 'TEST_ACTION_TRIGGER'
+
+    const TEST_ACTION = 'TEST_ACTION'
+
+    const childReducer = (state = 'initial value', action) => action.type === TEST_ACTION ? action.value : state
+    const parentReducer = combineReducers({ child1: childReducer, child2: namespaced('childNamespace')(childReducer) })
+    const rootReducer = combineReducers({ parent1: parentReducer, parent2: namespaced('parentNamespace')(parentReducer) })
+
+    const checkingEpic = (action$) => action$
+        .ofType(TEST_ACTION_TRIGGER)
+        ::map(action => ({ type: TEST_ACTION, value: action.value }))
+
+    it('should work with no subspaces', () => {
+        const rootStore = createStore(rootReducer, applyMiddleware(createEpicMiddleware(checkingEpic)))
+
+        rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+    })
+
+    it('should work with no namespace single subspace', () => {
+        const rootStore = createStore(rootReducer, applyMiddleware(createEpicMiddleware(checkingEpic)))
+
+        const parentStore = subspace((state) => state.parent1)(rootStore)
+
+        rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        parentStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'parent value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'parent value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+    })
+
+    it('should work with no namespace nested subspaces', () => {
+        const rootStore = createStore(rootReducer, applyMiddleware(createEpicMiddleware(checkingEpic)))
+
+        const parentStore = subspace((state) => state.parent1)(rootStore)
+
+        const childStore = subspace((state) => state.child1)(parentStore)
+
+        rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        parentStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'parent value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'parent value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        childStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'child value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'child value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+    })
+
+    it('should work with namespaced single subspace', () => {
+        const rootStore = createStore(rootReducer, applyMiddleware(createEpicMiddleware(
+            combineEpics(
+                checkingEpic,
+                subspaced((state) => state.parent2, 'parentNamespace')(checkingEpic)
+            )
+        )))
+
+        const parentStore = subspace((state) => state.parent2, 'parentNamespace')(rootStore)
+
+        rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        parentStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'parent value' })
+        
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'parent value',
+                child2: 'initial value'
+            }
+        })
+    })
+
+    it('should work with namespaced nested subspaces', () => {
+        const rootStore = createStore(rootReducer, applyMiddleware(createEpicMiddleware(
+            combineEpics(
+                checkingEpic,
+                subspaced((state) => state.parent2, 'parentNamespace')(
+                    combineEpics(
+                        checkingEpic,
+                        subspaced((state) => state.child2, 'childNamespace')(checkingEpic)
+                    )
+                )
+            )
+        )))
+
+        const parentStore = subspace((state) => state.parent2, 'parentNamespace')(rootStore)
+
+        const childStore = subspace((state) => state.child2, 'childNamespace')(parentStore)
+
+        rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        parentStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'parent value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'parent value',
+                child2: 'initial value'
+            }
+        })
+
+        childStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'child value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'parent value',
+                child2: 'child value'
+            }
+        })
+    })
+})
