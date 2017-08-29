e60fa6c4b589dacaed6921bb3605eb4258ae50dd
Riku Rouvila <riku.rouvila@gmail.com>
2017-08-29 21:36:16 +0100

    add more integration tests

--- packages/redux-subspace-loop/test/integration-spec.js
+++ packages/redux-subspace-loop/test/integration-spec.js
@@ -108,4 +108,82 @@ describe('integration tests', () => {
             }
         })
     })
+
+    it('should work with namespaced single subspace', async () => {
+        const rootStore = createStore(rootReducer, install())
+
+        const parentStore = subspace((state) => state.parent2, 'parentNamespace')(rootStore)
+
+        await rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        await parentStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'parent value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'parent value',
+                child2: 'initial value'
+            }
+        })
+    })
+
+    it('should work with namespaced nested subspace', async () => {
+        const rootStore = createStore(rootReducer, install())
+
+        const parentStore = subspace((state) => state.parent2, 'parentNamespace')(rootStore)
+        const childStore = subspace((state) => state.child2, 'childNamespace')(parentStore)
+
+        await rootStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'root value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'initial value',
+                child2: 'initial value'
+            }
+        })
+
+        await parentStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'parent value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'parent value',
+                child2: 'initial value'
+            }
+        })
+
+        await childStore.dispatch({ type: TEST_ACTION_TRIGGER, value: 'child value' })
+
+        expect(rootStore.getState()).to.deep.equal({
+            parent1: {
+                child1: 'root value',
+                child2: 'initial value'
+            },
+            parent2: {
+                child1: 'parent value',
+                child2: 'child value'
+            }
+        })
+    })
 })
