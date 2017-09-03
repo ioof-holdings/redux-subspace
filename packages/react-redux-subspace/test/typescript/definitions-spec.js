a8d2ff0b01815bf7ae42c619d6055f97c35be6a3
Michael Peyper <michael.peyper@ioof.com.au>
2017-09-04 08:17:32 +1000

    Increased timeout on typescript tests for travis build

--- packages/react-redux-subspace/test/typescript/definitions-spec.js
+++ packages/react-redux-subspace/test/typescript/definitions-spec.js
@@ -24,6 +24,6 @@ describe('TypeScript definitions', function () {
   fs.readdirSync(path.join(__dirname, 'definitions')).forEach((filename) => {
     it(`should compile ${path.basename(filename, path.extname(filename))} against index.d.ts`, (done) => {
       tt.compile([path.join(__dirname, 'definitions', filename)], options, done)
-    }).timeout(5000)
+    }).timeout(20000)
   });
 })
