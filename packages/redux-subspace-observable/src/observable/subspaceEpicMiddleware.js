990b1190f34077d3ba9dd4964a21dce820e6ebf1
Evert Bouw <bouw@outlook.com>
2018-01-21 19:50:51 +0100

    move store to a private key

--- packages/redux-subspace-observable/src/observable/subspaceEpicMiddleware.js
+++ packages/redux-subspace-observable/src/observable/subspaceEpicMiddleware.js
@@ -1,7 +1,15 @@
 import { applyToRoot } from 'redux-subspace'
 import { createEpicMiddleware as baseCreateEpicMiddleware } from 'redux-observable'
+import { SUBSPACE_STORE_KEY } from './subspaceStoreKey';
 
 export const createEpicMiddleware = (rootEpic, options = {}) =>
-    applyToRoot(store =>
-        baseCreateEpicMiddleware(rootEpic, { ...options, dependencies: { ...options.dependencies, store } })(store)
-    )
+    applyToRoot(store => {
+        const optionsWithStore = {
+            ...options,
+            dependencies: {
+                ...options.dependencies,
+                [SUBSPACE_STORE_KEY]: store
+            }
+        }
+        return baseCreateEpicMiddleware(rootEpic, optionsWithStore)(store)
+    })
