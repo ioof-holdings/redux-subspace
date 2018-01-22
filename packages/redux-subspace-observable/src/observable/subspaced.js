0d9b8b7c8f1720fdb04ecfb2cb424cc724dff5a8
Evert Bouw <e.bouw@cargoguide.com>
2018-01-22 10:05:42 +0100

    destructuring fails if param undefined

--- packages/redux-subspace-observable/src/observable/subspaced.js
+++ packages/redux-subspace-observable/src/observable/subspaced.js
@@ -16,7 +16,7 @@ const identity = (x) => x
 
 const subspaced = (mapState, namespace) => {
     const subspaceDecorator = subspace(mapState, namespace)
-    return epic => (action$, store, { [SUBSPACE_STORE_KEY]: parentStore, ...dependencies }) => {
+    return epic => (action$, store, { [SUBSPACE_STORE_KEY]: parentStore, ...dependencies } = {}) => {
         if (parentStore === undefined) {
             throw new Error('Subspace epic couldn\'t find the store. Make sure you\'ve used createEpicMiddleware from redux-subspace-observable')
         }
