c6f981d8f623369b170603306e4fc7a78fcc1e15
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-04-15 15:40:45 +0200

    Annotate pure calls with babel plugin to slightly improve tree-shakeability

--- packages/redux-subspace-observable/.babelrc.js
+++ packages/redux-subspace-observable/.babelrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'stage-3',
   ],
   plugins: [
+    'annotate-pure-calls',
     cjs && 'transform-es2015-modules-commonjs',
     'transform-function-bind'
   ].filter(Boolean),
