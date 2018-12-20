7586f1ff620e9b2d971838bc55bfc6363343590e
Michael Peyper <michael.peyper@ioof.com.au>
2018-12-20 20:46:11 +1100

    Publishing docs [ci skip]

--- packages/redux-subspace/src/actions/isGlobal.js
+++ packages/redux-subspace/src/actions/isGlobal.js
@@ -5,7 +5,8 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+const REDUX_PREFIX = '@@redux/'
 
-const isGlobal = (action) => !action.type || action.globalAction === true
+const isGlobal = (action) => !action.type || action.globalAction === true || action.type.startsWith(REDUX_PREFIX)
 
 export default isGlobal
