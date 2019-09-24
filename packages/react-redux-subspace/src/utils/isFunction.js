4e03cdfbbecf27e943e40fe8d95efbe97d47e140
Michael Peyper <michael.peyper@ioof.com.au>
2019-09-24 21:39:34 +1000

    Added some missing license headers

--- packages/react-redux-subspace/src/utils/isFunction.js
+++ packages/react-redux-subspace/src/utils/isFunction.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2017, IOOF Holdings Limited.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const isFunction = value => typeof value === "function"
 
 export default isFunction
--- packages/react-redux-subspace/src/utils/isObject.js
+++ packages/react-redux-subspace/src/utils/isObject.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2017, IOOF Holdings Limited.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const isObject = value => {
   const type = typeof value
   return type == "object" && value != null && !Array.isArray(value)
